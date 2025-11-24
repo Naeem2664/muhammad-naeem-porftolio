6f68086fa6bcec15bd4316322b6abd6b6d713061
Muhammad Naeem <faltooswag@gmail.com>
2025-11-24 21:09:23 +0500

    updated

--- components/sections/index/hero.jsx
+++ components/sections/index/hero.jsx
@@ -1,75 +1,79 @@
-import { useState } 		from 'react';
-import { TypeAnimation } 	from 'react-type-animation';
+import { useState } from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
-import Section 		from '../../structure/section';
-import Container 	from '../../structure/container';
+import Section from '../../structure/section';
+import Container from '../../structure/container';
 
-import space		from '../../utils/spacing.util';
+import space from '../../utils/spacing.util';
+import Icon from '../../utils/icon.util';
+import HeroBg from '../../blocks/hero.bg/bg-color-1';
 
-import Icon 		from '../../utils/icon.util'
-
-import HeroBg		from '../../blocks/hero.bg/bg-color-1';
-
-import hero 		from '../../../styles/sections/index/hero.module.scss';
-import button 		from '../../../styles/blocks/button.module.scss';
-
-import content		from '../../../content/index/hero.json'
-
+import hero from '../../../styles/sections/index/hero.module.scss';
+import button from '../../../styles/blocks/button.module.scss';
+import content from '../../../content/index/hero.json';
 
 export default function Hero() {
-
 	const [typingStatus, setTypingStatus] = useState('Initializing');
 
 	return (
-		<Section classProp={`${hero.section}`}>
-			<Container spacing={'VerticalXXXL'}>
-				<TypeAnimation className={`${hero.preHeader}`}
-					sequence={[
-						content.intro.startDelay,
-						() => { setTypingStatus('typing') },
-						content.intro.start,
-						() => {	setTypingStatus('typed') },
-						content.intro.deleteDelay,
-						() => {	setTypingStatus('deleting') },
-						content.intro.end,
-						() => {	setTypingStatus('deleted') },
-						content.intro.restartDelay,
-					]}
-					speed={content.intro.speed}
-					deletionSpeed={content.intro.deletionSpeed}
-					wrapper={content.intro.wrapper}
-					repeat={Infinity}
-				/>
-				<section>
-					<h1 className={hero.header}>
-						{content.header.name}
-						</h1>
-					<h1 className={`${hero.header} ${hero.primaryDim}`}>
-						{content.header.usp}
-					</h1>
-				</section>
-				<section>
-					<p className={`${hero.primaryBright} subtitle ${space(["verticalLrg"])}`}>
-						{ content.paragraph }
-					</p>					
-				</section>
-				<section>
-					<button	className={`button ${button.primary}`}
-							onClick={ () => window.location = 'mailto:m.naeem.4863@gmail.com' } >
-						{content.buttons.primary.title} <Icon icon={['fas', 'envelope']} />
-					</button>
-					<button className={`button ${button.primary}`}
-							onClick={ ()=> window.open("https://wa.me/923032664863", "_blank") } >
+		<Section classProp={hero.section}>
+			<div className={hero.backgroundWrapper}>
+				<Container classProp={hero.container} spacing={'VerticalXXXL'}>
+					<div className={hero.content}>
+						<div className={hero.typingContainer}>
+							<TypeAnimation 
+								className={hero.preHeader}
+								sequence={[
+									content.intro.startDelay,
+									() => { setTypingStatus('typing') },
+									content.intro.start,
+									() => { setTypingStatus('typed') },
+									content.intro.deleteDelay,
+									() => { setTypingStatus('deleting') },
+									content.intro.end,
+									() => { setTypingStatus('deleted') },
+									content.intro.restartDelay,
+								]}
+								speed={content.intro.speed}
+								deletionSpeed={content.intro.deletionSpeed}
+								wrapper={content.intro.wrapper}
+								repeat={Infinity}
+							/>
+						</div>
+						
+						<section className={hero.headerSection}>
+							<h1 className={hero.header}>
+								{content.header.name}
+							</h1>
+							<h1 className={`${hero.header} ${hero.primaryDim}`}>
+								{content.header.usp}
+							</h1>
+						</section>
+						
+						<section className={hero.paragraphSection}>
+							<p className={`${hero.primaryBright} ${hero.subtitle} ${space(["verticalLrg"])}`}>
+								{content.paragraph}
+							</p>
+						</section>
+						
+						<section className={hero.buttonSection}>
+							<button className={`${hero.button} ${button.primary}`} style={{padding: '0.9rem', fontSize: '1rem',borderRadius: '30px'}}
+									onClick={() => window.location = 'mailto:m.naeem.4863@gmail.com'}>
+								{content.buttons.primary.title} <Icon icon={['fas', 'envelope']} />
+							</button>
+							<button className={`${hero.button} ${button.primary}`} style={{padding: '0.9rem', fontSize: '1rem',borderRadius: '30px'}}
+									onClick={() => window.open("https://wa.me/923032664863", "_blank")}>
 								WhatsApp <Icon icon={['fab', 'whatsapp']} />
-					</button>
-					<button className={`button ${button.secondary}`}
-							onClick={ ()=> window.open("https://www.linkedin.com/in/muhammad-naeem-1b2b33219/", "_blank") } >
-						{content.buttons.secondary.title} <Icon icon={['fab', 'linkedin']} />
-					</button>
-
-				</section>
-			</Container>
-			<HeroBg theme="bg-color-1" />
+							</button>
+							<button className={`${hero.button} ${button.secondary}`} style={{padding: '0.9rem', fontSize: '1rem',borderRadius: '30px'}}
+									onClick={() => window.open("https://www.linkedin.com/in/muhammad-naeem-1b2b33219/", "_blank")}>
+								{content.buttons.secondary.title} <Icon icon={['fab', 'linkedin']} />
+							</button>
+						</section>
+					</div>
+				</Container>
+				<HeroBg theme="bg-color-1" />
+			</div>
 		</Section>
-	)
+	);
 }
